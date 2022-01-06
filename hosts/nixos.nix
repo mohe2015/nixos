e74e0989466e0b3c0e23eadd0f8d73809026f3bf
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-01-06 17:45:10 +0100

    networkd

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -432,7 +432,40 @@ args@{ self, lib, pkgs, nixpkgs, home-manager, config, agenix, release, home-man
 
   networking = {
     useDHCP = false;
-    networkmanager.enable = true;
+    useNetworkd = true;
+  };
+
+  systemd.network.networks = let
+    networkConfig = {
+      DHCP = "yes";
+      #DNSSEC = "yes";
+      #DNSOverTLS = "yes";
+      #DNS = [ "1.1.1.1" "1.0.0.1" ];
+    };
+  in {
+    # Config for all useful interfaces
+    "40-wired" = {
+      enable = true;
+      name = "en*";
+      inherit networkConfig;
+      dhcpV4Config.RouteMetric = 1024; # Better be explicit
+    };
+    "40-wireless" = {
+      enable = true;
+      name = "wl*";
+      inherit networkConfig;
+      dhcpV4Config.RouteMetric = 2048; # Prefer wired
+    };
+  };
+
+  # Wait for any interface to become available, not for all
+  systemd.services."systemd-networkd-wait-online".serviceConfig.ExecStart = [
+    "" "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any"
+  ];
+
+  #networking = {
+  #  useDHCP = false;
+    #networkmanager.enable = true;
     #interfaces.enp1s0 = {
     #  useDHCP = false;
     #  ipv4.addresses = [{
@@ -443,7 +476,7 @@ args@{ self, lib, pkgs, nixpkgs, home-manager, config, agenix, release, home-man
     #defaultGateway = "192.168.2.1";
     #search = [""];
     #nameservers = [ "8.8.8.8" "192.168.2.1" ];
-  };
+  #};
 
   #environment.etc."resolv.conf" = with lib; with pkgs; {
   #  source = writeText "resolv.conf" ''
